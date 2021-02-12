b776e25b76081601f1e1fdca8878077cee664c72
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-12 18:46:26 +0700

    Create Introduction to Data Science with R.R

---
+++ Learn/R Studio/Preliminary/Introduction to Data Science with R/Introduction to Data Science with R.R
@@ -0,0 +1,19 @@
+#"Hello World"
+"Hello World"
+
+#Number & Calculation Practice
+10+7
+
+#Variable in R
+a=5
+
+print(a)
+
+#Comment in R
+10+7 #This is comment row
+
+#Vector in R
+c(1:5)
+
+#Using Summary Function
+summary(c(1:5))
