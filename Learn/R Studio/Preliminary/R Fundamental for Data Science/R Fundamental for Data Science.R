f2f55b643edb9ed284dcd880ea1e906da699d7ae
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-12 19:58:50 +0700

    Create R Fundamental for Data Science.R

---
+++ Learn/R Studio/Preliminary/R Fundamental for Data Science/R Fundamental for Data Science.R
@@ -0,0 +1,100 @@
+#First Code, Hello World!(https://academy.dqlab.id/main/livecode/1/3/635) 
+"Hello World"
+
+"Hello World"
+1+5
+
+#[Texts, Numbers and Calculation Formula](https://academy.dqlab.id/main/livecode/1/3/6)
+9
+"Budi"
+
+9
+"Budi"
+9*3
+
+#[Displays with Print Function](https://academy.dqlab.id/main/livecode/1/3/705)
+print("Hello World")
+print(3+4)
+
+#[Uppercase, Lowercase, and Numbers Format](https://academy.dqlab.id/main/livecode/1/3/636)
+01
+1
+"01-01-1980"
+"1-1-1980"
+"Budi"
+"BUDI"
+
+#[Function](https://academy.dqlab.id/main/livecode/1/3/7)
+c(10:40)
+C(5:10)
+
+#[Variable](https://academy.dqlab.id/main/livecode/1/3/10)
+budi_berat_kg<-68
+santi_berat_kg<-54.5
+budi_berat_kg
+santi_berat_kg
+pi<-3.14
+pi
+
+#[Comment in R](https://academy.dqlab.id/main/livecode/1/3/9)
+2+2 #Ini adalah baris komentar
+#Ini adalah kode penutup
+
+#[Vector](https://academy.dqlab.id/main/livecode/1/4/14)
+#Ini adalah contoh vector untuk angka numerik dengan 3 data c(4, 5, 6)
+c(4, 5, 6)
+
+#Variable bernama angka dengan input berupa vector
+angka <-c(4, 5, 6)
+print(angka)
+
+#[Value Series with Operators](https://academy.dqlab.id/main/livecode/1/4/708)
+angka1 <-c(1,2,3,4,5,6,7,8,9,10)
+print(angka1)
+angka2 <-c(1:10)
+print(angka2)
+
+#[Vector with text](https://academy.dqlab.id/main/livecode/1/4/709)
+#variable nama_mahasiswa dengan input character
+nama_mahasiswa <-c("Amira", "Budi", "Charlie")
+print(nama_mahasiswa)
+
+#[Index and Accessor on Vector](https://academy.dqlab.id/main/livecode/1/4/15)
+# Buat vector variable bernama angka yang isinya 20 s/d 30
+angka <- c(20:30)
+print(angka)
+
+# Tampilkan isi variable angka pada posisi ke 3
+print(angka[3])
+
+# Tampilkan isi variable angka pada posisi ke 5
+print(angka[[5]])
+	  
+# Tampilkan isi variable angka pada posisi ke 4 s/d 6
+print(angka[4:6])
+
+# Buat vector teks dengan nama kode_prodi yang diisi sesuai petunjuk soal
+kode_prodi<-c("DKV", "ILKOM", "ICT")
+# Tampilkan isi indeks ketiga dari kode_prodi
+print(kode_prodi[3])
+
+#[Named Vector](https://academy.dqlab.id/main/livecode/1/4/16)
+#Membuat named vector dengan nama nilai
+nilai <- c(statistik = 89, fisika = 95, ilmukomunikasi = 100)
+
+#Menampilkan isi variable nilai
+print(nilai)
+
+#Menampilkan isi dengan nama fisika
+print(nilai["fisika"])
+
+#Buat variable profil sesuai permintaan soal
+profil<-c(nama="Budi", tempat_tinggal="Jakarta", tingkat_pendidikan="S1")
+
+#Tampilkan variable profil
+print(profil)
+
+#[List](https://academy.dqlab.id/main/livecode/1/4/17)
+
+
+
