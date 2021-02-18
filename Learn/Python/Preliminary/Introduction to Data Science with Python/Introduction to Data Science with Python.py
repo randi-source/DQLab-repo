3031795e7c1fbd8eee14c2449cf9094c5212359a
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-18 22:25:03 +0700

    Create Introduction to Data Science with Python.py

---
+++ Learn/Python/Preliminary/Introduction to Data Science with Python/Introduction to Data Science with Python.py
@@ -0,0 +1,20 @@
+#[Comments di Python](https://academy.dqlab.id/main/livecode/162/306/1401)
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/162/307/1406)
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+#[Menggunakan Library di Python](https://academy.dqlab.id/main/livecode/162/307/1408)
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
