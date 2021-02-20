631fb2b7613b4abd1a471b78468bdb0b46b599a8
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-21 00:15:11 +0700

    Update Python for Data Professional Beginner - Part 1.py

--- Learn/Python/Fundamental/Python for Data Professional Beginner - Part 1/Python for Data Professional Beginner - Part 1.py
+++ Learn/Python/Fundamental/Python for Data Professional Beginner - Part 1/Python for Data Professional Beginner - Part 1.py
@@ -12,44 +12,171 @@ print(contoh_tuple[0])
 contoh_tuple = ('Januari','Februari','Maret','April')
 contoh_tuple[0] = 'Desember'
 
-#[]()
+#[Set Type](https://academy.dqlab.id/main/livecode/157/284/1264)
+contoh_list = ['Dewi','Budi','Cici','Linda','Cici'] 
+print(contoh_list)
+contoh_set = {'Dewi','Budi','Cici','Linda','Cici'}
+print(contoh_set)
+contoh_frozen_set = ({'Dewi','Budi','Cici','Linda','Cici'})
+print(contoh_frozen_set)
 
+#[Mapping Type](https://academy.dqlab.id/main/livecode/157/284/1265)
+person = {'nama':'John Doe','pekerjaan':'Programmer'}
+print(person['nama'])
+print(person['pekerjaan'])
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/284/1266)
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = {"nama":"Baju Unikloh", "harga": 80000, "diskon":8000}
+celana = {"nama":"Celana Lepis", "harga": 200000, "diskon":60000}
 
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/284/1267)
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000} 
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000} 
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000} 
+daftar_belanja = [sepatu, baju, celana]
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/284/1268)
+# Data yang dinyatakan ke dalam dictionary
+sepatu = {"nama": "Sepatu Niko", "harga": 150000, "diskon": 30000} 
+baju = {"nama": "Baju Unikloh", "harga": 80000, "diskon": 8000} 
+celana = {"nama": "Celana Lepis", "harga": 200000, "diskon": 60000}
+# Hitunglah harga masing-masing data setelah dikurangi diskon
+harga_sepatu = sepatu["harga"] - sepatu["diskon"] 
+harga_baju = baju["harga"] - baju["diskon"]
+harga_celana = celana["harga"] - celana["diskon"]
+# Hitung harga total
+total_harga = harga_sepatu + harga_baju + harga_celana
+# Hitung harga kena pajak
+total_pajak = total_harga * 0.1
+# Cetak total_harga + total_pajak
+print(total_harga+total_pajak)
 
+#[Nilai Prioritas Operator dalam Python – Part 1](https://academy.dqlab.id/main/livecode/157/293/1295)
+# Kode awal
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = 1-0.3 # baris pertama
+harga_bayar *= total_harga # baris kedua
+pajak_bayar = pajak * harga_bayar # baris ketiga
+harga_bayar += pajak_bayar # baris ke-4
+print("Kode awal - harga_bayar=", harga_bayar)
+# Penyederhanaan baris kode dengan menerapkan prioritas operator
+total_harga = 150000
+potongan_harga = 0.3
+pajak = 0.1 # pajak dalam persen ~ 10%
+harga_bayar = (1-0.3) * total_harga #baris pertama 
+harga_bayar += harga_bayar * pajak # baris kedua
+print("Penyederhanaan kode - harga_bayar=", harga_bayar)
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/293/1298)
+sepatu = { "nama" : "Sepatu Niko", "harga": 150000, "diskon": 30000 }
+baju = { "nama" : "Baju Unikloh", "harga": 80000, "diskon": 8000 }
+celana = { "nama" : "Celana Lepis", "harga": 200000, "diskon": 60000 }
+harga_sepatu = sepatu["harga"] - sepatu["diskon"]
+harga_baju = baju["harga"] - baju["diskon"]
+harga_celana = celana["harga"] - celana["diskon"]
+total_harga = (harga_sepatu+harga_baju+harga_celana) * 1.1 
+print(total_harga)
 
+#[Python Conditioning for Decision – Part 2](https://academy.dqlab.id/main/livecode/157/294/1300)
+# Statement if
+x = 4
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+	print("x habis dibagi dua") # statemen aksi lebih menjorok ke dalam
+# Statement if ... elif ... else
+x = 7
+if x % 2 == 0: # jika sisa bagi x dengan 2 sama dengan 0
+	print("x habis dibagi dua")
+elif x % 3 == 0: # jika sisa bagi x dengan 3 sama dengan 0
+	print("x habis dibagi tiga")
+elif x % 5 == 0: # jika sisa bagi x dengan 5 sama dengan 0
+	print("x habis dibagi lima")
+else:
+	print("x tidak habis dibagi dua, tiga ataupun lima")
 
-#[]()
+#[Python Conditioning for Decision – Part 3](https://academy.dqlab.id/main/livecode/157/294/1301)
+jam = 13
+if jam >= 5 and jam < 12: # selama jam di antara 5 s.d. 12
+    print("Selamat pagi!")
+elif jam >= 12 and jam < 17: # selama jam di antara 12 s.d. 17
+    print("Selamat siang!")
+elif jam >= 17 and jam < 19: # selama jam di antara 17 s.d. 19
+    print("Selamat sore!")
+else: # selain kondisi di atas
+    print("Selamat malam!")
 
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/294/1302)
+tagihan_ke = 'Mr. Yoyo'
+warehousing = { 'harga_harian': 1000000, 'total_hari':15 } 
+cleansing = { 'harga_harian': 1500000, 'total_hari':10 } 
+integration = { 'harga_harian':2000000, 'total_hari':15 } 
+transform = { 'harga_harian':2500000, 'total_hari':10 }
+sub_warehousing = warehousing['harga_harian']*warehousing['total_hari']
+sub_cleansing = cleansing['harga_harian']*cleansing['total_hari'] 
+sub_integration = integration['harga_harian']*integration['total_hari'] 
+sub_transform = transform['harga_harian']*transform['total_hari']
+total_harga = sub_warehousing+sub_cleansing+sub_integration+sub_transform
+print("Tagihan kepada:") 
+print(tagihan_ke)
+print("Selamat pagi, anda harus membayar tagihan sebesar:") 
+print(total_harga)
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/294/1303)
+jam = 17
+tagihan_ke = 'Mr. Yoyo'
+warehousing = { 'harga_harian': 1000000, 'total_hari':15 } 
+cleansing = { 'harga_harian': 1500000, 'total_hari':10 } 
+integration = { 'harga_harian':2000000, 'total_hari':15 } 
+transform = { 'harga_harian':2500000, 'total_hari':10 }
+sub_warehousing = warehousing['harga_harian']*warehousing['total_hari'] 
+sub_cleansing = cleansing['harga_harian']*cleansing['total_hari'] 
+sub_integration = integration['harga_harian']*integration['total_hari'] 
+sub_transform = transform['harga_harian']*transform['total_hari']
+total_harga = sub_warehousing+sub_cleansing+sub_integration+sub_transform
+print("Tagihan kepada:")
+print(tagihan_ke)
+if jam > 19:
+    print("Selamat malam, anda harus membayar tagihan sebesar:")
+elif jam > 17:
+    print("Selamat sore, anda harus membayar tagihan sebesar:") 
+elif jam > 12:
+    print("Selamat siang, anda harus membayar tagihan sebesar:")
+else:
+    print("Selamat pagi, anda harus membayar tagihan sebesar:") 
+print(total_harga)
 
+#[Python while loops – Part 1](https://academy.dqlab.id/main/livecode/157/294/1305)
+# Tagihan
+tagihan = [50000, 75000, 125000, 300000, 200000]
+# Tanpa menggunakan while loop
+total_tagihan = tagihan[0]+tagihan[1]+tagihan[2]+tagihan[3]+tagihan[4]
+print(total_tagihan)
+# Dengan menggunakan while loop
+i = 0 # sebuah variabel untuk mengakses setiap elemen tagihan satu per satu
+jumlah_tagihan = len(tagihan) # panjang (jumlah elemen dalam) list tagihan
+total_tagihan = 0 # mula-mula, set total_tagihan ke 0
+while i < jumlah_tagihan: # selama nilai i kurang dari jumlah_tagihan
+    total_tagihan += tagihan [i] # tambahkan tagihan[i] ke total_tagihan
+    i += 1 # tambahkan nilai i dengan 1 untuk memproses tagihan selanjutnya.
+print(total_tagihan)
 
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
+#[Python while loops – Part 2](https://academy.dqlab.id/main/livecode/157/294/1306)
+tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+i = 0
+jumlah_tagihan = len(tagihan)
+total_tagihan = 0
+while i < jumlah_tagihan:
+    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+    # pengulangan akan dihentikan
+    if tagihan[i] < 0:
+        total_tagihan = -1
+        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan += tagihan[i]
+    i += 1
+print(total_tagihan)
 
 #[]()
 
