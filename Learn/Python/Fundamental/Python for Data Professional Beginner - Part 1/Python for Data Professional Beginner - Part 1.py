198dd62c6f1b2a1e3202b26075f4e498f7d64b1b
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-20 22:22:14 +0700

    Create Python for Data Professional Beginner - Part 1.py

---
+++ Learn/Python/Fundamental/Python for Data Professional Beginner - Part 1/Python for Data Professional Beginner - Part 1.py
@@ -0,0 +1,84 @@
+#[Sequence Type – Part 1](https://academy.dqlab.id/main/livecode/157/284/1262)
+contoh_list = [1,'dua',3,4.0,5]
+print(contoh_list[0])
+print(contoh_list[3])
+contoh_list = [1,'dua',3,4.0,5]
+contoh_list[3] = 'empat'
+print(contoh_list[3])
+
+#[Sequence Type – Part 2](https://academy.dqlab.id/main/livecode/157/284/1263)
+contoh_tuple = ('Januari','Februari','Maret','April')
+print(contoh_tuple[0])
+contoh_tuple = ('Januari','Februari','Maret','April')
+contoh_tuple[0] = 'Desember'
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
