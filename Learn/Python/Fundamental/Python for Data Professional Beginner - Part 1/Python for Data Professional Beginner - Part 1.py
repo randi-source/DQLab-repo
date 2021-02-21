70c18aa7724a222c15d249b1f695956686f52592
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-21 11:54:03 +0700

    Update Python for Data Professional Beginner - Part 1.py

--- Learn/Python/Fundamental/Python for Data Professional Beginner - Part 1/Python for Data Professional Beginner - Part 1.py
+++ Learn/Python/Fundamental/Python for Data Professional Beginner - Part 1/Python for Data Professional Beginner - Part 1.py
@@ -178,34 +178,82 @@ while i < jumlah_tagihan:
     i += 1
 print(total_tagihan)
 
-#[]()
+#[Python while loops – Part 3](https://academy.dqlab.id/main/livecode/157/294/1307)
+tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+i = 0
+jumlah_tagihan = len(tagihan)
+total_tagihan = 0
+while i < jumlah_tagihan:
+    # jika terdapat tagihan ke-i yang bernilai minus (di bawah nol),
+    # abaikan tagihan ke-i dan lanjutkan ke tagihan berikutnya
+    if tagihan[i] < 0:
+        i += 1
+        continue
+    total_tagihan += tagihan[i]
+    i += 1
+print(total_tagihan)
 
+#[Python for loops – Part 1](https://academy.dqlab.id/main/livecode/157/294/1308)
+list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+total_tagihan = 0
+for tagihan in list_tagihan: # untuk setiap tagihan dalam list_tagihan
+    total_tagihan += tagihan # tambahkan tagihan ke total_tagihan
+print(total_tagihan)
 
-#[]()
+#[Python for loops – Part 2](https://academy.dqlab.id/main/livecode/157/294/1309)
+list_tagihan = [50000, 75000, -150000, 125000, 300000, -50000, 200000]
+total_tagihan = 0
+for tagihan in list_tagihan:
+    if tagihan < 0:
+        print("terdapat angka minus dalam tagihan, perhitungan dihentikan!")
+        break
+    total_tagihan += tagihan
+print(total_tagihan)
 
+#[Python for loops – Part 3](https://academy.dqlab.id/main/livecode/157/294/1310)
+list_daerah = ['Malang', 'Palembang', 'Medan']
+list_buah = ['Apel', 'Duku', 'Jeruk']
+for nama_daerah in list_daerah:
+    for nama_buah in list_buah:
+        print(nama_buah+" "+nama_daerah)
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/157/294/1311)
+list_cash_flow = [
+2500000, 5000000, -1000000, -2500000, 5000000, 10000000,
+-5000000, 7500000, 10000000, -1500000, 25000000, -2500000
+]
+total_pengeluaran, total_pemasukan = 0, 0
+for dana in list_cash_flow:
+    if dana > 0:
+        total_pemasukan+= dana
+    else:
+        total_pengeluaran+=dana
+total_pengeluaran *= -1
+print(total_pengeluaran) 
+print(total_pemasukan)
 
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
+#[Ekspedisi Pamanku](https://academy.dqlab.id/main/livecode/157/295/1313)
+# Data
+uang_jalan = 1500000
+jumlah_hari = 31
+list_plat_nomor = [8993, 2198, 2501, 2735, 3772, 4837, 9152]
+# Pengecekan kendaraan dengan nomor pelat ganjil atau genap 
+kendaraan_genap = 0
+kendaraan_ganjil = 0 
+for plat_nomor in list_plat_nomor:
+    if plat_nomor % 2 == 0:
+        kendaraan_genap += 1 
+    else:
+        kendaraan_ganjil += 1
+# Total pengeluaran untuk kendaraan dengan nomor pelat ganjil
+# dan genap dalam 1 bulan
+i = 1
+total_pengeluaran = 0
+while i<= jumlah_hari:
+    if i % 2 == 0:
+        total_pengeluaran += (kendaraan_genap * uang_jalan) 
+    else:
+        total_pengeluaran += (kendaraan_ganjil * uang_jalan) 
+    i += 1
+# Cetak total pengeluaran
+print(total_pengeluaran)
