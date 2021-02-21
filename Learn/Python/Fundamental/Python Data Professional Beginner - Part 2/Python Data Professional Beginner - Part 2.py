7e89e0f5a1fab2a3862c0d3907cf2a1997c2f9ba
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-21 23:02:28 +0700

    Update Python Data Professional Beginner - Part 2.py

--- Learn/Python/Fundamental/Python Data Professional Beginner - Part 2/Python Data Professional Beginner - Part 2.py
+++ Learn/Python/Fundamental/Python Data Professional Beginner - Part 2/Python Data Professional Beginner - Part 2.py
@@ -82,26 +82,107 @@ print(">>> Fitur .count()")
 kemunculan_kata_apel = teks.count("apel")
 print(kemunculan_kata_apel)
 
-#[]()
+#[Menentukan String Apakah Diawali/Diakhiri oleh Sub-string](https://academy.dqlab.id/main/livecode/160/297/1336)
+# Fitur .startswith()
+print(">>> Fitur .startswith()")
+teks = "Apel malang adalah apel termanis dibanding apel-apel lainnya"
+print(teks.startswith("Apel"))
+print(teks.startswith("apel"))
+# Fitur .endswith()
+print(">>> Fitur .endswith()")
+print(teks.endswith("lainnya"))
+print(teks.endswith("apel"))
 
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/160/297/1337)
+judul_artikel = [
+"Buah Salak Baik untuk Mata", "Buah Salak Kaya Potasium", 
+"Buah Jeruk Kaya Vitamin C", "Buah Salak Kaya Manfaat", 
+"Salak Baik untuk Jantung", "Jeruk dapat Memperkuat Tulang", 
+"Jeruk Mencegah Penyakit Asma", "Jeruk Memperkuat Gigi", 
+"Jeruk Mencegah Kolesterol Jahat", "Salak Mencegah Diabetes", 
+"Salak Memperkuat Dinding Usus", "Salak Baik untuk Darah",
+"Jeruk Kaya Manfaat untuk Jantung", "Salak si Kecil yang Baik", 
+"Jeruk dan Salak Buah Kaya Manfaat", "Buah Jeruk Enak",
+"Tips Panen Jeruk Ribuan Kilo", "Tips Bertanam Salak", 
+"Salak Manis untuk Berbuka", "Jeruk Baik untuk Wajah"
+]
+jumlah_artikel_jeruk = 0
+jumlah_artikel_salak = 0
+for judul in judul_artikel:
+    if judul.count("Jeruk") > 0: 
+        jumlah_artikel_jeruk += 1
+    if judul.count("Salak") > 0:
+        jumlah_artikel_salak += 1
+print(jumlah_artikel_jeruk) 
+print(jumlah_artikel_salak)
 
-#[]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/160/297/1338)
+judul_artikel = [
+"Buah Salak Baik untuk Mata", "Buah Salak Kaya Potasium", 
+"Buah Jeruk Kaya Vitamin C", "Buah Salak Kaya Manfaat", 
+"Salak Baik untuk Jantung", "Jeruk dapat Memperkuat Tulang", 
+"Jeruk Mencegah Penyakit Asma", "Jeruk Memperkuat Gigi", 
+"Jeruk Mencegah Kolesterol Jahat", "Salak Mencegah Diabetes", 
+"Salak Memperkuat Dinding Usus", "Salak Baik untuk Darah",
+"Jeruk Kaya Manfaat untuk Jantung", "Salak si Kecil yang Baik", 
+"Jeruk dan Salak Buah Kaya Manfaat", "Buah Jeruk Enak",
+"Tips Panen Jeruk Ribuan Kilo", "Tips Bertanam Salak", 
+"Salak Manis untuk Berbuka", "Jeruk Baik untuk Wajah"
+]
+kata_positif = ["Kaya", "Baik", "Mencegah", "Memperkuat"]
+kata_positif_jeruk = 0
+kata_positif_salak = 0
+for judul in judul_artikel:
+	for kata in kata_positif:
+		if judul.count("Jeruk") > 0 and judul.count(kata) > 0:
+			kata_positif_jeruk += 1
+		if judul.count("Salak") > 0 and judul.count(kata) > 0:
+			kata_positif_salak += 1
+print(kata_positif_jeruk)
+print(kata_positif_salak)
 
+#[Fungsi Pertama](https://academy.dqlab.id/main/livecode/160/298/1340)
+# Definisikan fungsi
+def contoh_fungsi():
+    print("Halo Dunia")
+    print("Aku sedang belajar bahasa Python")
+# Panggil fungsi yang telah didefinisikan
+contoh_fungsi()
 
-#[]()
+#[Fungsi Kedua](https://academy.dqlab.id/main/livecode/160/298/1341)
+# Definsikan fungsi 
+def fungsi_dengan_argumen(nama_depan, nama_belakang):
+    print(nama_depan+ " " +nama_belakang)
+# Panggil fungsi dengan memasukkan argumen
+# nama_depan yaitu "John" dan nama_belakang "Doe"
+fungsi_dengan_argumen("John", "Doe")
 
+#[Fungsi Ketiga](https://academy.dqlab.id/main/livecode/160/298/1342)
+# Definsikan fungsi dengan nilai default argument kedua adalah "".
+def fungsi_dengan_argumen(nama_depan, nama_belakang=""):
+	print(nama_depan+ " " +nama_belakang)
+# Panggil fungsi dengan memasukkan argumen nama_depan "John"
+fungsi_dengan_argumen("John")
+# Panggil fungsi dengan memasukkan argumen
+# nama_depan yaitu "John" dan nama_belakang "Doe"
+fungsi_dengan_argumen("John", "Doe")
 
-#[]()
-
-
-#[]()
-
-
-#[]()
-
-
-#[]()
-
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/160/298/1343)
+# Dua buah data yang tersimpan dalam tipe list
+data1 = [70, 70, 70, 100, 100, 100, 120, 120, 150, 150]
+data2 = [50, 60, 60, 50, 70, 70, 100, 80, 100, 90]
+# Definisikan fungsi hitng_rata_rata
+def hitung_rata_rata(data):
+    jumlah = 0
+    for item in data:
+        jumlah += item
+    rata_rata = jumlah/len(data)
+    return rata_rata
+# Hitung nilai rata-rata dari kedua data yang dimiliki
+print('Rata-rata data1:')
+print(hitung_rata_rata(data1))
+print('Rata-rata data2:')
+print(hitung_rata_rata(data2))
 
 #[]()
 
