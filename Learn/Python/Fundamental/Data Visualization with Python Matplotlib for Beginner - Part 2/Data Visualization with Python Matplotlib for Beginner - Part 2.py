8f3f7688a088b8a40aa65c30dd986c50ffd56e2d
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-25 23:31:21 +0700

    Create Data Visualization with Python Matplotlib for Beginner - Part 2.py

---
+++ Learn/Python/Fundamental/Data Visualization with Python Matplotlib for Beginner - Part 2/Data Visualization with Python Matplotlib for Beginner - Part 2.py
@@ -0,0 +1,102 @@
+#[Membuat Multi-Line Chart](https://academy.dqlab.id/main/livecode/165/318/1483)
+# Import library
+import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+# Baca dataset
+dataset = pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced.csv')
+# Buat kolom baru yang bertipe datetime dalam format '%Y-%m'
+dataset['order_month'] = dataset['order_date'].apply(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d").strftime('%Y-%m'))
+# Buat Kolom GMV
+dataset['gmv'] = dataset['item_price']*dataset['quantity']
+
+# Buat Multi-Line Chart
+dataset.groupby(['order_month','brand'])['gmv'].sum().unstack().plot()
+plt.title('Monthly GMV Year 2019 - Breakdown by Brand', loc='center', pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (In Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.gcf().set_size_inches(10, 5)
+plt.tight_layout()
+plt.show()
+
+#[Kustomisasi Legend](https://academy.dqlab.id/main/livecode/165/318/1484)
+import matplotlib.pyplot as plt
+dataset.groupby(['order_month','brand'])['gmv'].sum().unstack().plot()
+plt.title('Monthly GMV Year 2019 - Breakdown by Brand',loc='center',pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize = 15)
+plt.ylabel('Total Amount (in Billions)',fontsize = 15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000).astype(int))
+plt.legend(loc='right', bbox_to_anchor=(1.6, 0.5), shadow= True, ncol=2)
+plt.gcf().set_size_inches(12, 5)
+plt.tight_layout()
+plt.show()
+
+#[Kustomisasi Colormap](https://academy.dqlab.id/main/livecode/165/318/1486)
+import matplotlib.pyplot as plt
+plt.clf()
+dataset.groupby(['order_month','province'])['gmv'].sum().unstack().plot(cmap='Set1')
+plt.title('Monthly GMV Year 2019 - Breakdown by Province', loc='center', pad=30, fontsize=20, color='blue')
+plt.xlabel('Order Month', fontsize=15)
+plt.ylabel('Total Amount (in Billions)', fontsize=15)
+plt.grid(color='darkgray', linestyle=':', linewidth=0.5)
+plt.ylim(ymin=0)
+labels, locations = plt.yticks()
+plt.yticks(labels, (labels/1000000000). astype(int))
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, -0.5), shadow=True, ncol=3, title='Province', fontsize=9, title_fontsize=11)
+plt.gcf().set_size_inches(10, 5)
+plt.tight_layout()
+plt.show()
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
+
+
+#[]()
