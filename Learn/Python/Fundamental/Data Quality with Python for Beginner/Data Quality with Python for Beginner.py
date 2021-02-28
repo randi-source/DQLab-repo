2e695893f2495c2369a743ea1abf4de505001e3d
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-28 14:02:36 +0700

    Update Data Quality with Python for Beginner.py

--- Learn/Python/Fundamental/Data Quality with Python for Beginner/Data Quality with Python for Beginner.py
+++ Learn/Python/Fundamental/Data Quality with Python for Beginner/Data Quality with Python for Beginner.py
@@ -1,46 +1,32 @@
-#[Importing Data]()
+#[Importing Data](https://academy.dqlab.id/main/livecode/166/322/1513)
 import pandas as pd
 import numpy as np
 import io
 import pandas_profiling
 retail_raw=pd.read_csv('https://dqlab-dataset.s3-ap-southeast-1.amazonaws.com/retail_raw_reduced_data_quality.csv')
 
-#[Inspeksi tipe data]()
+#[Inspeksi tipe data](https://academy.dqlab.id/main/livecode/166/322/1514)
 # Cetak tipe data di setiap kolom retail_raw
 print(retail_raw.dtypes)
 
-#[Tugas Praktek]()
+#[Descriptive Statistics - Part 1](https://academy.dqlab.id/main/livecode/166/322/1515)
 # Kolom city
 length_city=len(retail_raw['city'])
 print('Length kolom city:', length_city)
 
-# Tugas Praktek: Kolom product_id
-length_product_id = len(retail_raw['product_id'])
-print('Length kolom product_id:', length_product_id)
-
-#[Tugas Praktek]()
+#[Descriptive Statistics - Part 2](https://academy.dqlab.id/main/livecode/166/322/1517)
 # Count kolom city
 count_city=retail_raw['city'].count()
 print('Count kolom count_city:', count_city)
 
-# Tugas praktek: count kolom product_id
-count_product_id = retail_raw['product_id'].count()
-print('Count kolom product_id:', count_product_id)
-
-#[Tugas Praktek]()
+#[Descriptive Statistics - Part 3](https://academy.dqlab.id/main/livecode/166/322/1519)
 # Missing value pada kolom city
 number_of_missing_values_city = length_city-count_city
-float_of_missing_values_city= float(number_of_missing_values_city)/float(length_city)
+float_of_missing_values_city= float(number_of_missing_values_city/length_city)
 pct_of_missing_values_city='{0:.1f}%'.format(float_of_missing_values_city*100)
 print('Persentase missing value kolom city:', pct_of_missing_values_city)
 
-# Tugas praktek: Missing value pada kolom product_id
-number_of_missing_values_product_id = length_product_id-count_product_id
-float_of_missing_values_product_id = float(number_of_missing_values_product_id)/float(length_product_id)
-pct_of_missing_values_product_id = '{0:.1f}%'.format(float_of_missing_values_product_id * 100)
-print('Persentase missing value kolom product_id:', pct_of_missing_values_product_id)
-
-#[Tugas praktek]()
+#[Descriptive Statistics - Part 4](https://academy.dqlab.id/main/livecode/166/322/1521)
 # Deskriptif statistics kolom quantity
 print('Kolom quantity')
 print('Minimum value: ', retail_raw['quantity'].min())
@@ -50,16 +36,7 @@ print('Mode value: ', retail_raw['quantity'].mode())
 print('Median value: ', retail_raw['quantity'].median())
 print('Standard Deviation value: ', retail_raw['quantity'].std())
 
-# Tugas praktek: Deskriptif statistics kolom item_price
-print('')
-print('Kolom item_price')
-print('Minimum value: ', retail_raw['item_price'].min())
-print('Maximum value: ', retail_raw['item_price'].max())
-print('Mean value: ', retail_raw['item_price'].mean())
-print('Median value: ', retail_raw['item_price'].median())
-print('Standard Deviation value: ', retail_raw['item_price'].std())
-
-#[Tugas Praktek]()
+#[Descriptive Statistics - Part 5](https://academy.dqlab.id/main/livecode/166/322/1523)
 # Quantile statistics kolom quantity
 print('Kolom quantity:')
 print(retail_raw['quantity'].quantile([0.25, 0.5, 0.75]))
@@ -69,11 +46,11 @@ print('')
 print('Kolom item_price:')
 print(retail_raw['item_price'].quantile([0.25, 0.5, 0.75]))
 
-#[Tugas Praktek]()
+#[Descriptive Statistics - Part 6](https://academy.dqlab.id/main/livecode/166/322/1526)
 print('Korelasi quantity dengan item_price')
 print(retail_raw[['quantity', 'item_price']].corr())
 
-#[Missing Data]()
+#[Missing Data](https://academy.dqlab.id/main/livecode/166/323/1529)
 # Check kolom yang memiliki missing data
 print('Check kolom yang memiliki missing data:')
 print(retail_raw.isnull().any())
@@ -86,10 +63,10 @@ print(retail_raw['quantity'].fillna(retail_raw['quantity'].mean()))
 print('\nDrop missing value:')
 print(retail_raw['quantity'].dropna())
 
-#[Tugas Praktek]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/166/323/1530)
 print(retail_raw['item_price'].fillna(retail_raw['item_price'].mean()))
 
-#[Outliers]()
+#[Outliers](https://academy.dqlab.id/main/livecode/166/323/1534)
 # Q1, Q3, dan IQR
 Q1 = retail_raw['quantity'].quantile(0.25)
 Q3 = retail_raw['quantity'].quantile(0.75)
@@ -104,7 +81,7 @@ retail_raw = retail_raw[~((retail_raw['quantity'] < (Q1-1.5*IQR)) | (retail_raw[
 # Check ukuran (baris dan kolom) setelah data yang outliers dibuang
 print('Shape akhir: ', retail_raw.shape)
 
-#[Tugas Praktek]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/166/323/1535)
 # Q1, Q3, dan IQR
 Q1 = retail_raw['item_price'].quantile(0.25)
 Q3 = retail_raw['item_price'].quantile(0.75)
@@ -119,7 +96,7 @@ retail_raw = retail_raw[~((retail_raw['item_price'] < (Q1 - 1.5*IQR)) | (retail_
 # Check ukuran (baris dan kolom) setelah data yang outliers dibuang
 print('Shape akhir: ', retail_raw.shape)
 
-#[Tugas Praktek]()
+#[Tugas Praktek](https://academy.dqlab.id/main/livecode/166/323/1537)
 # Check ukuran (baris dan kolom) sebelum data duplikasi dibuang
 print('Shape awal: ', retail_raw.shape)
 
