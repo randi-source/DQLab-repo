5f23dd6dd2b7c06935a6b3d686368932dae01964
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-02-28 21:50:45 +0700

    Update Machine Learning With Python for Beginner.py

--- Learn/Python/Fundamental/Machine Learning With Python for Beginner/Machine Learning With Python for Beginner.py
+++ Learn/Python/Fundamental/Machine Learning With Python for Beginner/Machine Learning With Python for Beginner.py
@@ -111,26 +111,94 @@ dataset['VisitorType']=LE.fit_transform(dataset['VisitorType'])
 print(LE.classes_)
 print(np.sort(dataset['VisitorType'].unique()))
 
-#[]()
+#[Features & Label]()
+# removing the target column Revenue from dataset and assigning to X
+X = dataset.drop(['Revenue'], axis=1)
+# assigning the target column Revenue to y
+y = dataset['Revenue']
+# checking the shapes
+print("Shape of X:", X.shape)
+print("Shape of y:", y.shape)
 
+#[Training dan Test Dataset]()
+from sklearn.model_selection import train_test_split
+# splitting the X, and y
+X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.2, random_state=0)
+# checking the shapes
+print("Shape of X_train :", X_train.shape) 
+print("Shape of y_train :", y_train.shape)
+print("Shape of X_test :", X_test.shape)
+print("Shape of y_test :", y_test.shape)
 
-#[]()
+#[Training Model: Fit]()
+from sklearn.tree import DecisionTreeClassifier
+# Call the classifier
+model=DecisionTreeClassifier()
+# Fit the classifier to the training data
+model=model.fit(X_train, y_train)
 
+#[Training Model: Predict]()
+# Apply the classifier/model to the test data
+y_pred=model.predict(X_test)
+print(y_pred.shape)
 
-#[]()
+#[Evaluasi Model Performance - Part 2]()
+from sklearn.metrics import confusion_matrix, classification_report
 
+# evaluating the model
+print('Training Accuracy :', model.score(X_train, y_train))
+print('Testing Accuracy :', model.score(X_test, y_test))
 
-#[]()
+# confusion matrix
+print('\nConfusion matrix:')
+cm=confusion_matrix(y_test, y_pred)
+print(cm)
 
+# classification report
+print('\nClassification report:')
+cr=classification_report(y_test, y_pred)
+print(cr)
 
-#[]()
+#[Pemodelan Permasalahan Klasifikasi dengan Logistic Regression]()
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, classification_report
 
+# Call the classifier
+logreg = LogisticRegression()
+# Fit the classifier to the training data  
+logreg = logreg.fit(X_train, y_train)
+#Training Model: Predict 
+y_pred = logreg.predict(X_test)
 
-#[]()
+#Evaluate Model Performance
+print('Training Accuracy :', model.score(X_train, y_train))  
+print('Testing Accuracy :', model.score(X_test, y_test))  
 
+# confusion matrix
+print('\nConfusion matrix')  
+cm = confusion_matrix(y_test, y_pred)  
+print(cm)
 
-#[]()
+# classification report  
+print('\nClassification report')  
+cr = classification_report(y_test, y_pred)  
+print(cr)
 
+#[Classification - Decision Tree]()
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+# splitting the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+# Call the classifier
+decision_tree = DecisionTreeClassifier()
+# Fit the classifier to the training data
+decision_tree = decision_tree.fit(X_train, y_train)
+
+# evaluating the decision_tree performance
+print('Training Accuracy :', decision_tree.score(X_train, y_train))
+print('Testing Accuracy :', decision_tree.score(X_test, y_test))
 
 #[]()
 
