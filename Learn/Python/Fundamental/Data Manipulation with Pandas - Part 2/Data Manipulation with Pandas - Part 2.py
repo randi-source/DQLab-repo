7dbf57a3c371ec334ff9b69b51df2d24c8fdccaa
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-03-03 01:45:25 +0700

    Update Data Manipulation with Pandas - Part 2.py

--- Learn/Python/Fundamental/Data Manipulation with Pandas - Part 2/Data Manipulation with Pandas - Part 2.py
+++ Learn/Python/Fundamental/Data Manipulation with Pandas - Part 2/Data Manipulation with Pandas - Part 2.py
@@ -138,11 +138,48 @@ print('Creating pivot table -- aggfunc mean:\n', pivot_tab_mean)
 pivot_tab_median = data.pivot_table(index='kelas', columns='pelajaran', values='nilai', aggfunc='median')
 print('Creating pivot table -- aggfunc median:\n', pivot_tab_median)
 
-#[]()
+#[Melt - Part 1]()
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Pivoting dataframe
+data_pivot = data.pivot_table(index='kelas', columns='pelajaran', values='nilai', aggfunc='mean').reset_index()
+print('Pivoting dataframe:\n', data_pivot)
+# [1] Melting dataframe data_pivot
+data_melt_1 = pd.melt(data_pivot)
+print('Melting dataframe:\n', data_melt_1)
+# [2] Melting dataframe data_pivot dengan id_vars
+data_melt_2 = pd.melt(data_pivot, id_vars='kelas')
+print('Melting dataframe dengan idvars:\n', data_melt_2)
 
-#[]()
+#[Melt - Part 2]()
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Pivoting dataframe
+data_pivot = data.pivot_table(index='kelas',columns='pelajaran',values='nilai', aggfunc='mean').reset_index()
+print('Pivoting dataframe:\n', data_pivot)
+# [3.a] Melting dataframe data_pivot dengan value_vars
+data_melt_3a = pd.melt(data_pivot, value_vars=['math'])
+print('Melting dataframe dengan value_vars:\n', data_melt_3a)
+# [3.b] Melting dataframe data_pivot dengan id_vars dan value_vars
+data_melt_3b = pd.melt(data_pivot, id_vars='kelas', value_vars=['math'])
+print('Melting dataframe dengan id_vars dan value_vars:\n', data_melt_3b)
+# [4] Melting dataframe data_pivot dengan id_vars, value_vars, var_name. dan value_name
+data_melt_4 = pd.melt(data_pivot, id_vars='kelas', value_vars=['english','math'], var_name='pelajaran', value_name='nilai')
+print('Melting dataframe dengan id_vars, value_vars, var_name. dan value_name:\n', data_melt_4)
 
-#[]()
+#[Stack & Unstack - Part 1]()
 
 #[]()
 
