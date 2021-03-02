c8e783e58c1cd2104eb2d98b1f5f55c5a2e6e09b
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-03-02 18:27:39 +0700

    Create Data Manipulation with Pandas - Part 2.py

---
+++ Learn/Python/Fundamental/Data Manipulation with Pandas - Part 2/Data Manipulation with Pandas - Part 2.py
@@ -0,0 +1,177 @@
+#[Append]()
+import pandas as pd
+# Buat series of int (s1) dan series of string (s2)
+s1 = pd.Series([1,2,3,4,5,6])
+s2 = pd.Series(["a","b","c","d","e","f"])
+# Terapkan method append
+s2_append_s1 = s2.append(s1)
+print("Series - append:\n", s2_append_s1)
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+		         'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+		         'a':[3,4]})
+# Terapkan method append
+df2_append_df1 = df2.append(df1)
+print("Dataframe - append:\n", df2_append_df1)
+
+#[Concat]()
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({'a':[1,2],
+					'b':[3,4]})
+df2 = pd.DataFrame({'b':[1,2],
+					'a':[3,4]})
+# Terapkan method concat row-wise
+row_wise_concat = pd.concat([df2, df1])
+print("Row-wise - concat:\n", row_wise_concat)
+# Terapkan method concat column-wise
+col_wise_concat = pd.concat([df2, df1], axis=1)
+print("Column-wise - concat:\n", col_wise_concat)
+# Penambahan identifier --> membentuk hasil penggabungan multiindex
+multiindex_concat = pd.concat([df2, df1], axis=0, keys=['df1','df2'])
+print("Multiindex - concat:\n", multiindex_concat)
+
+#[Merge - Part 1]()
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+# Merge yang ekivalen dengan SQL left join
+merge_df_left = pd.merge(left=df2, right=df1, how='left', left_on='key', right_on='key')
+print('Merge - Left:\n', merge_df_left)
+# Merge yang ekivalen dengan SQL right join
+merge_df_right = pd.merge(left=df2, right=df1, how='right', left_on='key', right_on='key')
+print('Merge - Right:\n', merge_df_right)
+# Merge yang ekivalen dengan SQL inner join
+merge_df_inner = pd.merge(left=df2, right=df1, how='inner', left_on='key', right_on='key')
+print('Merge - Inner:\n', merge_df_inner)
+# Merge yang ekivalen dengan SQL outer join
+merge_df_outer = pd.merge(left=df2, right=df1, how='outer', left_on='key', right_on='key')
+print('Merge - Outer:\n', merge_df_outer)
+
+#[Merge - Part 2]()
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+}).set_index(['key','val2'])
+print('Dataframe 1:\n', df1)
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+}).set_index(['key','val3'])
+print('Dataframe 2:\n', df2)
+# Merge dataframe yang memiliki multi index
+df_merge = pd.merge(df1.reset_index(), df2.reset_index())
+print('Merging dataframe:\n', df_merge)
+
+#[Join]()
+import pandas as pd
+# Buat dataframe df1 dan df2
+df1 = pd.DataFrame({
+   'key':['k1','k2','k3','k4','k5'],
+   'val1':[200, 500, 0, 500, 100],
+   'val2':[30, 50, 100, 20, 10]
+})
+df2 = pd.DataFrame({
+   'key':['k1','k3','k5','k7','k10'],
+   'val3':[1,2,3,4,5],
+   'val4':[6,7,8,8,10]
+})
+# Penerapan join dengan menggunakan set_index dan keyword how
+join_df = df1.set_index('key').join(df2.set_index('key'), how='outer')
+print(join_df)
+
+#[Dataset]()
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Unique value pada setiap kolom data
+for column in data.columns:
+    print('Unique value %s: %s' % (column, data[column].unique()))
+
+#[Pivot]()
+import pandas as pd
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Pivoting with single column measurement
+pivot1 = data.pivot(index='murid', columns='pelajaran', values='nilai')
+print('Pivoting with single column measurement:\n', pivot1)
+# Pivoting with multiple column measurement
+pivot2 = data.pivot(index='murid', columns='pelajaran')
+print('Pivoting with multiple column measurement:\n', pivot2)
+
+#[Pivot_table]()
+# Dataframe
+data = pd.DataFrame({
+  'kelas': 6*['A'] + 6*['B'],
+  'murid': 2*['A1'] + 2*['A2'] + 2*['A3'] + 2*['B1'] + 2*['B2'] + 2*['B3'],
+  'pelajaran': 6*['math','english'],
+  'nilai': [90,60,70,85,50,60,100,40,95,80,60,45]
+}, columns=['kelas','murid','pelajaran','nilai'])
+# Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='mean'
+pivot_tab_mean = data.pivot_table(index='kelas', columns='pelajaran', values='nilai', aggfunc='mean')
+print('Creating pivot table -- aggfunc mean:\n', pivot_tab_mean)
+# Creating pivot and assign pivot_tab dengan menggunakan keyword aggfunc='median'
+pivot_tab_median = data.pivot_table(index='kelas', columns='pelajaran', values='nilai', aggfunc='median')
+print('Creating pivot table -- aggfunc median:\n', pivot_tab_median)
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
+
+#[]()
