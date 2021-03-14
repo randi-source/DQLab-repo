09d5aed3b3e4b2b2bca670ebaf7b2d559f1a74de
Randi Eka Sanjaya <78034231+randi-source@users.noreply.github.com>
2021-03-14 21:29:13 +0700

    Update Fundamental SQL with SELECT Statement.sql

--- Learn/SQL/Preliminary/Fundamental SQL with SELECT Statement/Fundamental SQL with SELECT Statement.sql
+++ Learn/SQL/Preliminary/Fundamental SQL with SELECT Statement/Fundamental SQL with SELECT Statement.sql
@@ -1,98 +1,98 @@
 #[Perintah SELECT [2/2]](https://academy.dqlab.id/main/livecode/91/213/1086)
 SELECT 
-	* 
+    * 
 FROM 
-   ms_produk;
+    ms_produk;
 
 #[Mengambil Satu Kolom dari Table](https://academy.dqlab.id/main/livecode/91/214/1093)
 SELECT 
-   nama_produk 
+    nama_produk 
 FROM 
-   ms_produk;
+    ms_produk;
 
 #[Mengambil Lebih dari Satu Kolom](https://academy.dqlab.id/main/livecode/91/214/1095)
 SELECT
-   nama_produk, 
-   harga 
+    nama_produk, 
+    harga 
 FROM 
-   ms_produk;
+    ms_produk;
 
 #[Membatasi Pengambilan Jumlah Row Data](https://academy.dqlab.id/main/livecode/91/214/1097)
 SELECT 
-   nama_produk 
+    nama_produk 
 FROM 
-   ms_produk LIMIT 3;	
+    ms_produk LIMIT 3;	
 
 #[Menggunakan Prefix pada Nama Kolom](https://academy.dqlab.id/main/livecode/91/215/1100)
 SELECT 
-   ms_produk.kode_produk 
+    ms_produk.kode_produk 
 FROM 
-   ms_produk;
+    ms_produk;
 
 #[Menggunakan Alias pada Kolom](https://academy.dqlab.id/main/livecode/91/215/1102)
 SELECT 
-	no_urut AS nomor,
-	nama_produk AS nama 
+    no_urut AS nomor,
+    nama_produk AS nama 
 FROM 
-	ms_produk;
+    ms_produk;
 
 #[Menghilangkan Keyword 'AS'](https://academy.dqlab.id/main/livecode/91/215/1104)
 SELECT 
-	no_urut nomor,
-	nama_produk nama 
+    no_urut nomor,
+    nama_produk nama 
 FROM 
-	ms_produk;
+    ms_produk;
 
 #[Menggabungkan Prefix dan Alias](https://academy.dqlab.id/main/livecode/91/215/1106)
 SELECT 
-	ms_produk.harga AS harga_jual 
+    ms_produk.harga AS harga_jual 
 FROM 
-	ms_produk;
+    ms_produk;
 
 #[Menggunakan Alias pada Table](https://academy.dqlab.id/main/livecode/91/215/1107)
 SELECT 
-	* 
+    * 
 FROM 
-	ms_produk t2;
+    ms_produk t2;
 
 #[Prefix dengan Alias Table](https://academy.dqlab.id/main/livecode/91/215/1108)
 SELECT 
-	t2.nama_produk, 
-	t2.harga 
+    t2.nama_produk, 
+    t2.harga 
 FROM 
-	ms_produk t2;
+    ms_produk t2;
 
 #[Menggunakan WHERE](https://academy.dqlab.id/main/livecode/91/216/1111)
 SELECT 
-	* 
+    * 
 FROM 
-	ms_produk 
+    ms_produk 
 WHERE 
-	nama_produk='Tas Travel Organizer DQLab';
+    nama_produk='Tas Travel Organizer DQLab';
 
 #[Menggunakan Operand OR](https://academy.dqlab.id/main/livecode/91/216/1112)
 SELECT 
-	* 
+    * 
 FROM 
-	ms_produk 
+    ms_produk 
 WHERE 
-  nama_produk='Gantungan Kunci DQLab' OR 
-  nama_produk='Tas Travel Organizer DQLab' OR 
-  nama_produk='Flashdisk DQLab 64 GB';
+    nama_produk='Gantungan Kunci DQLab' OR 
+    nama_produk='Tas Travel Organizer DQLab' OR 
+    nama_produk='Flashdisk DQLab 64 GB';
 
 #[Filter untuk Angka](https://academy.dqlab.id/main/livecode/91/216/1113)
 SELECT 
-	* 
+    * 
 FROM 
-	ms_produk 
+    ms_produk 
 WHERE 
-	harga>50000;
+    harga>50000;
 
 #[Menggunakan Operand AND](https://academy.dqlab.id/main/livecode/91/216/1114)
 SELECT 
-	* 
+    * 
 FROM 
-	ms_produk 
+    ms_produk 
 WHERE 
-   nama_produk='Gantungan Kunci DQLab' AND 
-	harga<50000;
+    nama_produk='Gantungan Kunci DQLab' AND 
+    harga<50000;
